817fdc89f0e00c2aa9fe0ce123376834133b6a98
jack-and-his-ants <jack.piano.ant47@gmail.com>
2026-06-10 01:23:13 +0200

    feat: add evaluation notebook for the new best model

--- src/inference/colorize.py
+++ src/inference/colorize.py
@@ -85,54 +85,54 @@ def colorize_image(
         reconstructed_rgb,
         pred_classes
     )
+if __name__=="__main__":
+    device = get_device()
 
-device = get_device()
+    model = ColorizationNetwork(
+        n_classes=32
+    )
 
-model = ColorizationNetwork(
-    n_classes=32
-)
+    checkpoint = torch.load(
+        "/Users/jacek/Desktop/GitHubProjects/pictures-coloring/notebooks/checkpoints/best_model.pth",
+        map_location=device
+    )
 
-checkpoint = torch.load(
-    "/Users/jacek/Desktop/GitHubProjects/pictures-coloring/notebooks/checkpoints/best_model.pth",
-    map_location=device
-)
+    model.load_state_dict(
+        checkpoint["model_state_dict"]
+    )
 
-model.load_state_dict(
-    checkpoint["model_state_dict"]
-)
+    model.to(device)
 
-model.to(device)
+    kmeans = joblib.load(
+        "/Users/jacek/Desktop/GitHubProjects/pictures-coloring//test_kmeans.pkl"
+    )
 
-kmeans = joblib.load(
-    "/Users/jacek/Desktop/GitHubProjects/pictures-coloring//test_kmeans.pkl"
-)
+    original, colorized, classes = colorize_image(
+        "/Users/jacek/Desktop/GitHubProjects/pictures-coloring/data/test/Image_15.jpg",
+        model,
+        kmeans,
+        device=device
+    )
 
-original, colorized, classes = colorize_image(
-    "/Users/jacek/Desktop/GitHubProjects/pictures-coloring/data/test/Image_15.jpg",
-    model,
-    kmeans,
-    device=device
-)
+    plt.figure(figsize=(12, 5))
 
-plt.figure(figsize=(12, 5))
+    plt.subplot(1, 3, 1)
+    plt.imshow(original)
+    plt.title("Original")
+    plt.axis("off")
 
-plt.subplot(1, 3, 1)
-plt.imshow(original)
-plt.title("Original")
-plt.axis("off")
+    plt.subplot(1, 3, 2)
+    plt.imshow(
+        color.rgb2gray(original),
+        cmap="gray"
+    )
+    plt.title("Input L")
+    plt.axis("off")
 
-plt.subplot(1, 3, 2)
-plt.imshow(
-    color.rgb2gray(original),
-    cmap="gray"
-)
-plt.title("Input L")
-plt.axis("off")
+    plt.subplot(1, 3, 3)
+    plt.imshow(colorized)
+    plt.title("Colorized")
+    plt.axis("off")
 
-plt.subplot(1, 3, 3)
-plt.imshow(colorized)
-plt.title("Colorized")
-plt.axis("off")
-
-plt.show()
+    plt.show()
 
